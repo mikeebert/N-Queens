74362db8ce829a597dce5b19216e51959b2d4966
mikeebert <mike@mikeebert.net>
2012-04-16 15:39:50 -0500

    Follow up to initial commit to make sure all files are added.

---
+++ lib/queensolver.rb
@@ -0,0 +1,209 @@
+class QueenSolver
+  
+  attr_accessor :board
+  attr_accessor :size
+  attr_accessor :positions
+  attr_accessor :row
+  attr_accessor :column
+  
+  def initialize(n)
+    if n > 3 or n == 1
+      @size = n
+      set_board
+      set_initial_location
+      @positions = Array.new
+    else
+      display_error(n)
+    end
+  end
+  
+  def set_initial_location
+    @row = 0; @column = 0
+  end
+
+  def place_queens
+    place_next_queen
+    display_board
+  end
+  
+  def place_next_queen
+    @positions << [@row,@column]
+    place_positions unless @positions.empty?
+    @row += 1
+    @column = 0
+    print "#{@positions} \n"
+    coordinates_for_next_move unless @positions.count == @size
+  end
+  
+  def coordinates_for_next_move
+    while column_is_occupied(@column) == true || diagonal_is_occupied(@row,@column) == true
+      @column += 1
+      if @column == @size
+        if @positions.last[1] == @size - 1
+          @positions.delete_at(@positions.count - 1)
+          @positions.delete_at(@positions.count - 1)            
+        else
+          @row = @positions.last[0]
+          @column = @positions.last[1] + 1
+          @positions.delete_at(@positions.count - 1)
+        end
+        if @positions.count == 0 
+          @row = 0
+          @column += 1
+        end
+      end
+    end
+    place_next_queen
+  end
+  
+  def column_is_occupied(column_index)
+    column = @board.map {|row| row[column_index]}
+    return true if column.compact.count > 0
+  end
+  
+  def diagonal_is_occupied(row_index, column_index)
+    #check up and back
+    row = row_index - 1
+    column = column_index - 1
+    while row >=0 && column >= 0
+      return true if @board[row][column] == "Q"
+      row -= 1; column -= 1
+    end
+
+    #check down and forward
+    row = row_index + 1
+    column = column_index + 1
+    while row < @size && column < @size
+      return true if @board[row][column] == "Q"
+      row += 1;column += 1
+    end
+    
+    #check up and forward
+    row = row_index - 1
+    column = column_index + 1
+    while row > 0 && column < @size
+      return true if @board[row][column] == "Q"
+      row -= 1;column +=1
+    end
+    
+    #check down and back
+    row = row_index + 1
+    column = column_index - 1
+    while row < @size && column > 0
+      return true if @board[row][column] == "Q"
+      row += 1;column -= 1
+    end
+  end
+  
+  def solved
+    all_queens_placed && attacks_possible != true
+  end
+  
+  def all_queens_placed
+    @board.flatten.compact.count.should == @size
+  end
+  
+  def attacks_possible  
+    return true if horizontal_attack == true
+    return true if vertical_attack == true
+    return true if diagonal_attack == true
+  end
+
+  def horizontal_attack
+    @board.each do |row|
+      return true if row.compact.count > 1
+    end
+  end
+
+  def vertical_attack
+    column_values = (0..(@size-1)).to_a
+    column_values.each do |column_position|
+      column = @board.map {|row| row[column_position]}
+       return true if column.compact.count > 1
+     end
+  end
+  
+  def diagonal_attack
+    if forward_slash_attack == true
+      return true
+    elsif backward_slash_attack == true
+      return true
+    end
+  end
+  
+  # def forward_slash_attack
+  #   columns = (0..(@size-2)).to_a
+  #   columns.each do |column|
+  #     @row = 0; @column = column
+  #     forward_slash = []
+  #     (@size - column).times do
+  #       forward_slash << @board[@row][@column]
+  #       return true if forward_slash.compact.count > 1
+  #       @row += 1
+  #       @column += 1
+  #     end
+  #   end
+  #   
+  #   rows = (0..(@size-2)).to_a
+  #   rows.each do |row|
+  #     @row = row; @column = 0
+  #     forward_slash = []
+  #     (@size - row).times do
+  #       forward_slash << @board[@row][@column]
+  #       return true if forward_slash.compact.count > 1
+  #       @row += 1
+  #       @column += 1
+  #     end
+  #   end
+  # end
+  # 
+  # def backward_slash_attack
+  #   columns = (1..(@size-1)).to_a
+  #   columns.reverse.each do |column|
+  #     @row = 0; @column = column
+  #     backward_slash = []
+  #     (column + 1).times do
+  #       backward_slash << @board[@row][@column]
+  #       return true if backward_slash.compact.count > 1
+  #       @row += 1
+  #       @column -= 1
+  #     end
+  #   end
+  #   
+  #   rows = (0..(@size-2)).to_a
+  #   rows.each do |row|
+  #     @row = row; @column = (@board.count -1)
+  #     backward_slash = []
+  #     (@size - row).times do
+  #       backward_slash << @board[@row][@column]
+  #       return true if backward_slash.compact.count > 1
+  #       @row += 1
+  #       @column -= 1
+  #     end
+  #   end
+  # end
+  
+  def display_error(n)
+    puts "The N Queens problem is not possible for a #{n} x #{n} board."
+  end
+  
+  def set_board
+    @board = Array.new(@size) 
+    @board.map! {|row| row = Array.new(@size)}
+  end
+  
+  def place_positions
+    set_board
+    display_board
+    print "The positions are: #{positions} \n"
+    @positions.each do |coordinates|
+      @board[coordinates[0]][coordinates[1]] = "Q"
+    end    
+  end
+  
+  def display_board
+    @board.each do |row|
+      puts "#{row} \n"
+    end
+  end
+end
---
+++ spec/queensolver_spec.rb
@@ -0,0 +1,162 @@
+require 'queensolver'
+
+describe "Solving the N Queens Puzzle" do
+  
+  describe "setting up the board" do
+    it "should create a board for 1" do
+      @queen_solver = QueenSolver.new(1)
+      @queen_solver.board.should == [[nil]]
+    end
+
+    it "should create a board for any number greater than 3" do
+      @queen_solver2 = QueenSolver.new(4)
+      @queen_solver2.board.should == [[nil,nil,nil,nil],
+                                     [nil,nil,nil,nil],
+                                     [nil,nil,nil,nil],
+                                     [nil,nil,nil,nil]]
+    end
+    
+    it "should set Queens from an array of positions" do
+      @queen_solver = QueenSolver.new(4)
+      @queen_solver.positions = [[0,0],[0,3],[3,0],[3,3]]
+      @queen_solver.place_positions
+      @queen_solver.board.should == [["Q",nil,nil,"Q"],
+                                     [nil,nil,nil,nil],
+                                     [nil,nil,nil,nil],
+                                     ["Q",nil,nil,"Q"]]
+    end
+  end
+  
+  describe "checking for attacks" do
+    before(:each) do
+      @queen_solver = QueenSolver.new(4)
+    end
+    
+    it "should know if a horizontal attack is possible" do
+      @queen_solver.board[0] = ["Q","Q",nil,nil]
+      @queen_solver.horizontal_attack.should == true
+      @queen_solver.attacks_possible.should == true
+    end
+    
+    it "should know if a vertical attack is possible" do
+      @queen_solver.board[0][0] = "Q"
+      @queen_solver.board[1][0] = "Q"
+      @queen_solver.vertical_attack.should == true
+      @queen_solver.attacks_possible.should == true
+    end
+    
+    it "should catch a forward-slash diagonal attack in a top corner" do
+      @queen_solver.board = [[nil,nil,"Q",nil],
+                             [nil,nil,nil,"Q"],
+                             [nil,nil,nil,nil],
+                             [nil,nil,nil,nil]]
+      @queen_solver.forward_slash_attack.should == true
+      @queen_solver.attacks_possible.should == true
+    end
+    
+    it "should catch a forward-slash diagonal attack in a bottom corner" do
+      @queen_solver.board = [[nil,nil,nil,nil],
+                             ["Q",nil,nil,nil],
+                             [nil,nil,nil,nil],
+                             [nil,nil,"Q",nil]]
+      @queen_solver.forward_slash_attack.should == true
+      @queen_solver.attacks_possible.should == true
+    end
+
+    it "should catch a backward-slash attack in a top corner" do
+      @queen_solver.board = [[nil,"Q",nil,nil],
+                             ["Q",nil,nil,nil],
+                             [nil,nil,nil,nil],
+                             [nil,nil,nil,nil]]
+      @queen_solver.backward_slash_attack.should == true
+      @queen_solver.diagonal_attack.should == true
+      @queen_solver.attacks_possible.should == true
+    end
+    
+    it "should find a backward-slash attack in a bottom corner" do
+      @queen_solver.board = [[nil,nil,nil,nil],
+                             [nil,nil,nil,"Q"],
+                             [nil,nil,nil,nil],
+                             [nil,"Q",nil,nil]]
+      @queen_solver.backward_slash_attack.should == true
+      @queen_solver.diagonal_attack.should == true
+      @queen_solver.attacks_possible.should == true     
+    end
+    
+    it "should not return true for an empty board" do
+      @queen_solver.horizontal_attack.should_not == true
+      @queen_solver.vertical_attack.should_not == true
+      @queen_solver.diagonal_attack.should_not == true
+      @queen_solver.attacks_possible.should_not == true
+    end
+  end
+  
+  describe "checking for a solved baord" do
+    before(:each) do
+      @queen_solver = QueenSolver.new(4)
+      @queen_solver.board = [[nil,"Q",nil,nil],
+                             [nil,nil,nil,"Q"],
+                             ["Q",nil,nil,nil],
+                             [nil,nil,"Q",nil]]      
+    end
+    
+    it "should know that n queens have been placed" do
+      @queen_solver.all_queens_placed.should == true
+    end
+    
+    it "should know that a board has been solved" do
+      @queen_solver.solved.should == true
+    end
+  end
+  
+  describe "placing queens on a baord" do
+    
+    it "should return the coordinates for the next move" do
+      
+    end
+    
+    
+    it "should know if a column is occupied" do
+      @queen_solver = QueenSolver.new(4)
+      @queen_solver.board = [["Q",nil,nil,nil],
+                             [nil,nil,nil,nil],
+                             [nil,nil,nil,nil],
+                             [nil,nil,nil,nil]]
+      @queen_solver.column_is_occupied(0).should == true
+    end
+    
+    it "should know when a diagonal is occupied" do
+      @queen_solver = QueenSolver.new(4)
+      @queen_solver.board = [[nil,nil,nil,nil],
+                             [nil,"Q",nil,nil],
+                             [nil,nil,nil,nil],
+                             [nil,nil,nil,nil]]
+      @queen_solver.diagonal_is_occupied(0,0).should == true
+      @queen_solver.diagonal_is_occupied(2,0).should == true      
+    end
+    
+    it "should move a queen to the first available space" do
+      @queen_solver = QueenSolver.new(4)
+      @queen_solver.board[0][0] = "Q"
+      @queen_solver.positions << [0,0]
+      @queen_solver.move_to_next_available_space
+      @queen_solver.board.should == [["Q",nil,nil,nil],
+                                     [nil,nil,"Q",nil],
+                                     [nil,nil,nil,nil],
+                                     [nil,nil,nil,nil]]
+    end
+    
+    it "should place 1 Queen on a 1 x 1 Board" do
+      @queen_solver = QueenSolver.new(1)
+      @queen_solver.place_queens
+      @queen_solver.solved.should == true
+    end
+    
+    it "should place 4 Queens on a 4 x 4 Board" do
+      @queen_solver = QueenSolver.new(4)
+      @queen_solver.place_queens
+      # @queen_solver.solved.should == true
+    end
+  end
+  
+end
